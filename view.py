9b211b27317450d1cd9724feeb6f13fc5ac413d4
nySophia2019 <hg1533@nyu.edu>
2023-07-13 07:30:07 -0700

    3rd commit

---
+++ view.py
@@ -0,0 +1,9 @@
+# Test scripts
+import pip
+	
+	pip install pandas openpyxl
+	pip install plotly-express
+	pip install streamlit
+	
+	python "D:/WorkSpace/Python/SalesDashboard/"app.py            # run the sales dashboard scripts
+	streamlit run ./Common/app.py     # to view this Streamlit app on a browser
